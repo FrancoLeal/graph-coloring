d525b9f31e182f3f6b107bc1270466e2a0641164
Franco <franco.leal@usach.cl>
2020-07-20 19:09:34 -0400

    arreglos

--- neighborFunctions.py
+++ neighborFunctions.py
@@ -1,6 +1,8 @@
 import random
+import numpy as np
 
-def createNeighboor1(solution):
+def createNeighboor1(solutionOriginal):
+	solution = np.copy(solutionOriginal)
 	lenSolution = len(solution)
 	x1,x2 = random.sample(list(range(0,lenSolution)),2)
 	
@@ -10,8 +12,9 @@ def createNeighboor1(solution):
 	solution[x2] = aux
 	return solution
 
-def createNeighboor2(problem,solution):
+def createNeighboor2(problem,solutionOriginal):
 	value = 1
+	solution = np.copy(solutionOriginal)
 	lenSolution = len(solution)
 	while value != 0:
 		node1,node2 = random.sample(list(range(0,lenSolution)),2)
